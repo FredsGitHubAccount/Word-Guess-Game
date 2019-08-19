8e8a58c84bab31b9cbc58f678d6735f2a89874b2
Fred Saba <fred@fred-saba.com>
2019-08-19 11:07:41 -0700

    updated documentation

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,6 @@
-// // 1. Have a list of words that can be guessed & variables
+// Global Variables
 
-// list of characters
+// List of characters
 let nintendoChars = ["mario", "luigi", "peach", "bowser", "toad", "blooper", "goomba", "yoshi", "boo", "waluigi", "wario", "toadsworth", "rosalina", "daisy", "koopalings", "birdo", "kamek", "toadette", "piranha", "koopa", "wiggler", "pokey", "morton"]
 
 // Computer randomly picks a word from the list
@@ -28,45 +28,49 @@ let losses = 0;
 let guessesRemain = 10;
 var audio = new Audio('Assets/music/mario.mp3');
 
-//    2. The computer randomly picks a word from the list and breaks down the letters into an array
+// Function to start a new round
 function newGame() {
 
-
+    // Select a random word from the nintendoChars variable
     computerGuess = nintendoChars[Math.floor(Math.random() * nintendoChars.length)];
     console.log(computerGuess);
 
-    computerGuessLetters = computerGuess.split(""); // computerGuess split into individual letters inside an array
+    // Split the random word inside an array
+    computerGuessLetters = computerGuess.split("");
     console.log(computerGuessLetters)
 
-    totalBlanks = computerGuess.length; // Total amount of underscores that will be pushed into displayBlanks
-    console.log(totalBlanks);
+    // Determine the total amount of blanks needed based on the length of the random word
+    totalBlanks = computerGuess.length;
 
-    //    3. Generate underscores based on the length of the word
+
+    // Clear variables 
     displayBlanks = [];
     wrongLetters = [];
     guessesRemain = 10;
     userGuessHolder = []
 
+    // Push the amount of underscores into the document and into the displayBlanks variable
     for (let i = 0; i < totalBlanks; i++) {
         (displayBlanks.push("_"));
 
     }
+    // Dom manipulation 
     document.getElementById("generate-underscore").innerHTML = displayBlanks.join(" ");
     document.getElementById("guesses-remaining").innerHTML = `Remaining Guesses : ${guessesRemain}`;
 
 }
 
-//    4. Verify if keystroke is part of the alphabet.  Checks to see if letter is part of the word.  If so, replace underscore with letter.  If not, adds to wrong guesses and subtracts a guess.
-
+//    Function to verify the key that was pressed
 function letterChecker(guess) {
     audio.play()
 
-
+    // Checks to see if the key pressed was part of the alphabet
     if (event.keyCode >= 65 && event.keyCode <= 90) {
 
+        // By default, the letterInWord is false and will toggle true if the letter matches
         let letterInWord = false;
 
-
+        // Run a for loop to see if the user guess matches the index of the computerguess
         for (let i = 0; i < totalBlanks; i++) {
             if (computerGuess[i] === guess) {
                 letterInWord = true;
@@ -74,14 +78,15 @@ function letterChecker(guess) {
             }
         }
 
-
+        // If the letter matches an index of the word, run through this statement
         if (letterInWord) {
 
+            // Checks to see if the user already picked that letter in the userGuessHolder array
             if (userGuessHolder.includes(guess)) {
                 document.getElementById("alerts").innerHTML = "You've Already Picked That Letter!"
 
             }
-
+            // Runs a for loop to turn the underscore into the corresponding letter and pushes the guess into the userGuessHolder array
             for (let i = 0; i < totalBlanks; i++) {
 
                 if (computerGuess[i] === guess) {
@@ -95,23 +100,26 @@ function letterChecker(guess) {
         }
 
 
-
+        // If the guess is wrong run the following statements
         else {
-
+            // Checks to see if the guess was already given
             if (wrongLetters.includes(guess)) {
                 document.getElementById("alerts").innerHTML = "Don't Pick The Same Letter Twice! If I Was A Mean Game Developer, I'd Subtract A Guess!"
 
             }
-
+            // If it was a new wrong guess, then subtract a guess and push the letter into the wrongLetters array
             else {
                 wrongLetters.push(guess);
                 guessesRemain--;
             }
 
         }
+        // Dom manipulation
         document.getElementById("wrong-guess-text").innerHTML = `Wrong Guesses : ${wrongLetters.join(" , ")}`;
         document.getElementById("guesses-remaining").innerHTML = `Remaining Guesses : ${guessesRemain}`;
         document.getElementById("directions-text").innerHTML = 'Good Luck!';
+        
+        // Alerts to hit a valid key
     } else {
 
         document.getElementById("alerts").innerHTML = "Make Sure To Hit A Valid Key!"
@@ -119,7 +127,7 @@ function letterChecker(guess) {
 
 };
 
-//    5. If user guesses all letters, a win is added, & randomly select another word. If 5 wins or 5 losses occurs, prompt the user to play again.  
+//   Function to check if the user has guessed the entire word & to check if the user has reached 5 wins or 5 losses  
 function roundComplete() {
 
     if (computerGuessLetters == displayBlanks.toString()) {
@@ -157,6 +165,7 @@ function roundComplete() {
 
 }
 
+// Function to run the game from scratch with default menu
 function freshStart() {
 
     wins = 0;
@@ -171,7 +180,7 @@ function freshStart() {
 
 }
 
-// Calling Functions
+// Called Functions
 newGame();
 document.onkeyup = function (event) {
     document.getElementById("alerts").innerHTML = ""
